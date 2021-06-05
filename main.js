dd9e2cb04f83587a9baf897d97b5779b0a85a95e
Ray from Digify <rl-digify@protonmail.com>
2021-06-05 03:43:09 +0200

    added content, styling and animated stars

--- main.js
+++ main.js
@@ -32,6 +32,21 @@ pointLight.position.set(5, 5, 5)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
 
+function addStar() {
+	const geometry = new THREE.SphereGeometry(0.05, 24, 24)
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
+	const star = new THREE.Mesh(geometry, material)
+
+	const [x, y, z] = Array(3)
+		.fill()
+		.map(() => THREE.MathUtils.randFloatSpread(100))
+
+	star.position.set(x, y, z)
+	scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
 // Bg
 
 const spaceTexture = new THREE.TextureLoader().load(
